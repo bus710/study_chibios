50b4192851a9545537c884da9cd1c58478686d66
Seongjun Kim <bus710+github@gmail.com>
2016-10-16 16:13:49 -0700

    .

--- README_10.md
+++ README_10.md
@@ -1,4 +1,4 @@
-[표지로 돌아가기](README.md)
+[표지로 돌아가기](README.md#howwhat---어떻게-무엇을-개발하고-공유할까)
 
 # study_chibios - download and installation
   
---
+++ projects/RT-STM32F407-DISCOVERY-01/.dep/crt1.o.d
@@ -0,0 +1,50 @@
+build/obj/crt1.o: \
+ C:/ChibiStudio/chibios161/os/common/ports/ARMCMx/compilers/GCC/crt1.c \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\lib\gcc\arm-none-eabi\4.9.3\include\stdbool.h \
+ C:/ChibiStudio/chibios161/os/common/ports/ARMCMx/devices/STM32F4xx/cmparams.h \
+ C:/ChibiStudio/chibios161/os/hal/boards/ST_STM32F4_DISCOVERY/board.h \
+ C:/ChibiStudio/chibios161/os/ext/CMSIS/ST/STM32F4xx/stm32f4xx.h \
+ C:/ChibiStudio/chibios161/os/ext/CMSIS/ST/STM32F4xx/stm32f407xx.h \
+ C:/ChibiStudio/chibios161/os/ext/CMSIS/include/core_cm4.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\lib\gcc\arm-none-eabi\4.9.3\include\stdint.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\stdint.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\machine\_default_types.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\sys\features.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\sys\_intsup.h \
+ c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\sys\_stdint.h \
+ C:/ChibiStudio/chibios161/os/ext/CMSIS/include/core_cmInstr.h \
+ C:/ChibiStudio/chibios161/os/ext/CMSIS/include/core_cmFunc.h \
+ C:/ChibiStudio/chibios161/os/ext/CMSIS/include/core_cmSimd.h \
+ C:/ChibiStudio/chibios161/os/ext/CMSIS/ST/STM32F4xx/system_stm32f4xx.h
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\lib\gcc\arm-none-eabi\4.9.3\include\stdbool.h:
+
+C:/ChibiStudio/chibios161/os/common/ports/ARMCMx/devices/STM32F4xx/cmparams.h:
+
+C:/ChibiStudio/chibios161/os/hal/boards/ST_STM32F4_DISCOVERY/board.h:
+
+C:/ChibiStudio/chibios161/os/ext/CMSIS/ST/STM32F4xx/stm32f4xx.h:
+
+C:/ChibiStudio/chibios161/os/ext/CMSIS/ST/STM32F4xx/stm32f407xx.h:
+
+C:/ChibiStudio/chibios161/os/ext/CMSIS/include/core_cm4.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\lib\gcc\arm-none-eabi\4.9.3\include\stdint.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\stdint.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\machine\_default_types.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\sys\features.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\sys\_intsup.h:
+
+c:\chibistudio\tools\gnu\ tools\ arm\ embedded\4.9\ 2015q3\arm-none-eabi\include\sys\_stdint.h:
+
+C:/ChibiStudio/chibios161/os/ext/CMSIS/include/core_cmInstr.h:
+
+C:/ChibiStudio/chibios161/os/ext/CMSIS/include/core_cmFunc.h:
+
+C:/ChibiStudio/chibios161/os/ext/CMSIS/include/core_cmSimd.h:
+
+C:/ChibiStudio/chibios161/os/ext/CMSIS/ST/STM32F4xx/system_stm32f4xx.h:
