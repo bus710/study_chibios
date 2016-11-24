1f5b8cae97c22cbd7da2d77f44185efc1945f4f5
Seongjun Kim <bus710+github@gmail.com>
2016-11-23 20:37:49 -0800

    .

--- projects_esc/p003/hwconf/hw_410.c
+++ projects_esc/p003/hwconf/hw_410.c
@@ -58,6 +58,7 @@ void hw_init_gpio(void) {
 			PAL_STM32_OSPEED_HIGHEST);
 
 	// GPIOA Configuration: Channel 1 to 3 as alternate function push-pull
+	// PWM outputs
 	palSetPadMode(GPIOA, 8, PAL_MODE_ALTERNATE(GPIO_AF_TIM1) |
 			PAL_STM32_OSPEED_HIGHEST |
 			PAL_STM32_PUDR_FLOATING);
